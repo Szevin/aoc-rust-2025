2919736f1563151d2f3ca45e8401adfccfa7ed3b
Geiger-Ötvös Kevin <kevingeiger25@gmail.com>
2025-12-04 09:52:41 +0100

    refactor day 4 part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,12 +1,39 @@
-use std::u32::MAX;
-
 use ndarray::Array2;
-use rayon::{iter::{IntoParallelRefIterator, ParallelIterator}, str::ParallelString};
+use rayon::{iter::{ParallelIterator}, str::ParallelString};
 
 advent_of_code::solution!(4);
 
-fn increase_neighbours(weight_map: &mut Array2<u32>, x: usize, y: usize) {
-  let indicies = vec![
+struct Coord {
+  x: i32,
+  y: i32,
+}
+
+impl From<(usize, usize)> for Coord {
+  fn from((x, y): (usize, usize)) -> Self {
+    Coord::new(x as i32, y as i32)
+  }
+}
+
+impl Clone for Coord {
+    fn clone(&self) -> Self {
+        Self { x: self.x.clone(), y: self.y.clone() }
+    }
+}
+
+impl Coord {
+  pub fn new(x: i32, y: i32)-> Self {
+    Coord{x, y}
+  }
+
+  fn to_usize(&self) -> Option<(usize, usize)> {
+    match self.x >= 0 && self.y >= 0 {
+        true => Some((self.y as usize, self.x as usize)),
+        false => None
+    }
+  }
+
+  fn generate_neighbour_coords(&self) -> Vec<Coord> {
+    vec![
     (-1 as i32, 1 as i32),
     (0, 1),
     (1, 1),
@@ -17,69 +44,108 @@ fn increase_neighbours(weight_map: &mut Array2<u32>, x: usize, y: usize) {
     (1, -1),
   ]
   .iter()
-  .map(|(x1, y1)| (x1 + x as i32, y1 + y as i32))
-  .filter_map(|(x1, y1)| match x1 >= 0 && y1 >= 0 {
-    true => Some((x1 as usize, y1 as usize)),
-    false => None,
-  })
-  .collect::<Vec<(usize, usize)>>();
-
-  indicies
-    .iter()
-    .filter_map(|(x1, y1)| weight_map.get_mut_ptr((*y1, *x1)))
-    .for_each(|value| unsafe {
-      *value += 1;
-    });
+  .map(|(x, y)| Coord::new(x + self.x as i32, y + self.y as i32))
+  .collect::<Vec<Coord>>()
+  }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-  let lines = input.par_lines().collect::<Vec<&str>>();
-  let width = lines.first().unwrap().len();
-  let height = lines.len();
+enum Tile {
+  Empty(Coord),
+  Paper(Coord),
+}
 
-  let bit_mask = Array2::<u32>::from_shape_vec(
-    (height, width),
-    lines
-      .par_iter()
-      .flat_map_iter(|s| s.chars())
-      .map(|c| if c == '@' { 1 } else { 0 })
-      .collect(),
-  )
-  .unwrap();
-
-  let mut weight_map = Array2::<u32>::zeros((width, height));
-
-  for (y, line) in lines.iter().enumerate() {
-    for (x, char) in line.chars().enumerate() {
-      match char {
-        '@' => {
-          increase_neighbours(&mut weight_map, x, y);
-        }
-        _ => {}
+impl Tile {
+    pub fn new(c: char, coord: Coord) -> Self {
+      match c {
+          '@' => Self::Paper(coord),
+          _ => Self::Empty(coord)
       }
     }
-  }
 
-  weight_map.zip_mut_with(&bit_mask, |d, &m| match m {
-      0 => {
-        *d = MAX
+    fn get_coord(&self) -> Coord {
+      match self {
+          Tile::Paper(coord) => coord.clone(),
+          Tile::Empty(coord) => coord.clone()
       }
-      _ => {
-        *d *= m
+    }
+
+    fn is_empty(&self) -> bool {
+      match self {
+          Tile::Paper(_) => false,
+          Tile::Empty(_) => true
       }
-  });
-
-  // dbg!(&weight_map);
-
-  (weight_map
-  .iter()
-  .map(|&x| x)
-  .filter(|&x| x < 4)
-  .count() as u32)
-  .into()
+    }
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+struct Floor {
+  grid: Array2<Tile>
+}
+
+impl Floor {
+    pub fn new(input: &str)-> Self {
+        let lines = input.par_lines().collect::<Vec<&str>>();
+        let width = lines.first().unwrap().len();
+        let height = lines.len();
+
+        let grid = Array2::<Tile>::from_shape_vec(
+          (height, width),
+          lines
+          .iter().enumerate()
+          .map(|(y, line)| {
+            line.chars().enumerate().map(|(x, char)| {
+              Tile::new(char, Coord::from((x, y)))
+            })
+            .collect::<Vec<Tile>>()
+          })
+          .flatten()
+          .collect()
+        )
+        .unwrap();
+
+        Floor { grid }
+    }
+
+    fn get_neighbouring_tiles(&self, tile: &Tile) -> Vec<&Tile> {
+      let neighbour_coords = tile.get_coord().generate_neighbour_coords();
+
+      neighbour_coords
+      .iter()
+      .filter_map(|coord| {
+        coord.to_usize()
+      })
+      .filter_map(|coord| {
+        self.grid.get(coord)
+      })
+      .collect::<Vec<&Tile>>()
+    }
+
+    fn count_not_empty_neighbours(&self, tile: &Tile) -> usize {
+      self
+      .get_neighbouring_tiles(tile)
+      .iter()
+      .filter(|neighbour| !neighbour.is_empty())
+      .count()
+    }
+
+    fn eliminate(&self) -> usize {
+      self.grid
+        .iter()
+        .filter(|&tile| !tile.is_empty())
+        .map(|tile| {
+          match self.count_not_empty_neighbours(tile) < 4 {
+              true => 1,
+              false => 0
+          }
+        })
+        .sum()
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+  Floor::new(input).eliminate().into()
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
   None
 }
 
